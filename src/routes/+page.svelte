49e561e99ba4b91acba3f34ca91a3137564a51a4
Stu C1 <79088483+stuc-aerstudios@users.noreply.github.com>
2023-09-07 21:35:17 +0100

    commit all

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,95 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <script>
+	import { baseData } from '../stores.js';
+	import { DateTime } from 'luxon';
+
+	const getTeamGames = (teamShort) => {
+		return baseData.games.filter((game) => {
+			return game.teams[0].team === teamShort || game.teams[1].team === teamShort;
+		});
+	};
+
+	export const poolColors = {
+		A: { color: 'var(--pool-a)', textColor: 'black' },
+		B: { color: 'var(--pool-b)', textColor: 'black' },
+		C: { color: 'var(--pool-c)', textColor: 'white' },
+		D: { color: 'var(--pool-d)', textColor: 'white' },
+		Q: { color: '#000044', textColor: 'white' },
+		S: { color: '#808080', textColor: 'white' },
+		R: { color: '#cd7f32', textColor: 'white' },
+		F: { color: '#daa520', textColor: 'white' }
+	};
+
+	export const players = {};
+	for (const team of baseData.teams) {
+		if (!players[team.personshort]) {
+			players[team.personshort] = {
+				full: team.personlong,
+				initials: team.personshort,
+				teams: []
+			};
+		}
+
+		const games = getTeamGames(team.short);
+
+		players[team.personshort].teams.push({
+			team: team.long,
+			code: team.short,
+			flag: team.icon,
+			flagCode: team.code,
+			pool: {
+				...baseData.pools.find((poolTeam) => poolTeam.team === team.short),
+				pool: team.pool,
+				colors: { ...poolColors[team.pool] }
+			},
+			seed: team.rating,
+			games,
+			getNextGame() {
+				return this?.games?.find((game) => {
+					return DateTime.fromFormat(game.date, 'yyyy-MM-dd HH:mm') > DateTime.now();
+				});
+			}
+		});
+	}
+
+	export const pools = {};
+	for (const team of baseData.pools) {
+		if (!pools[team.pool]) {
+			pools[team.pool] = {
+				pool: team.pool,
+				colors: poolColors[team.pool],
+				teams: []
+			};
+		}
+
+		pools[team.pool].teams.push({
+			...team,
+			player: Object.values(players).find((player) => player.full === team.player)
+		});
+	}
+
+	export const games = [];
+	for (const game of baseData.games) {
+		game.pool = { pool: game.pool, colors: poolColors[game.pool] };
+		games.push(game);
+	}
+
+	export const playersSelect = Object.keys(players).map((key) => {
+		return {
+			value: key,
+			label: players[key].full
+		};
+	});
+
+	export const teams = {};
+	for (const player of Object.values(players)) {
+		for (const team of player.teams) {
+			teams[team.code] = { ...team, player };
+		}
+	}
+
 	import '../app.css';
-	import { playersSelect, players, pools, games, formattedDate, teams } from '$lib';
 	import GameCard from '../components/GameCard.svelte';
 	import GamesTable from '../components/GamesTable.svelte';
 	import PoolTable from '../components/PoolTable.svelte';
@@ -32,7 +119,6 @@
 		});
 
 		selectedGame = { game, pool, teams: gameTeams };
-		console.log(selectedGame);
 	}
 </script>
 
