892b8a659107dc31ef2061b983c188e3ecc6c693
Stu C1 <79088483+stuc-aerstudios@users.noreply.github.com>
2023-09-07 23:54:17 +0100

    update

--- src/stores.js
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-import { localStore } from "./localStore.js";
-import data from './lib/data.json';
-
-export const alert = writable("Using the initial data");
-export const baseData = localStore("rwc-data", data);
