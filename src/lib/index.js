fa325693f1121a06529b40c5fd6434165ba0e9aa
Stu C1 <79088483+stuc-aerstudios@users.noreply.github.com>
2023-09-07 21:38:15 +0100

    initial of this

--- src/lib/index.js
+++
@@ -1,6 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-import { DateTime } from "luxon"
-
-export const formattedDate = (dateStr, formatOutput = 'EEE dd MMM, HH:mm') => {
-    return DateTime.fromFormat(dateStr, 'yyyy-MM-dd HH:mm').toFormat(formatOutput)
-}
