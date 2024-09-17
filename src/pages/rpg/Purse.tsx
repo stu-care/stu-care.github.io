efe7c8e937938a60f0fec5a4a7a4f1df42eeb9b4
Stu C1 <79088483+stuc-aerstudios@users.noreply.github.com>
2024-09-17 15:30:07 +0100

    Update purse functionality

--- src/pages/rpg/Purse.tsx
+++ src/pages/rpg/Purse.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { currency, Rates } from "../../helpers/currency";
+import { currency, Rates, CoinDenominations } from "../../helpers/currency";
 import { useApp } from "../../contexts/AppContext";
 import { homeTitle } from "../Home";
 import { rpgTitle } from "../RPG";
@@ -16,6 +16,26 @@ export const purseTitle = (
         Purse
     </span>
 );
+
+const denominations: CoinDenominations[] = [
+    "mp",
+    "gp",
+    "sp",
+    "bp",
+    "cp",
+    "tp",
+    "ip",
+];
+
+const valuesInIp: { [key in CoinDenominations]: number } = {
+    mp: 10000000,
+    gp: 100000,
+    sp: 10000,
+    bp: 1000,
+    cp: 100,
+    tp: 10,
+    ip: 1,
+};
 
 const PursePage = () => {
     const { purse } = useRPG();
@@ -38,33 +58,101 @@ const PursePage = () => {
         breadcrumbs.add({ url: "/rpg/purse", label: purseTitle });
     }, []);
 
-    const handleCurrencyChange = (abbr: string, value: number) => {
-        console.log(abbr, value);
+    const handleCurrencyChange = (abbr: CoinDenominations, value: number) => {
         purse.set({ ...purse.values, [abbr]: value || 0 });
     };
 
     useEffect(() => {
-        console.log(purse.values, totalCurrency);
         const totalInGold = Object.entries(purse.values).reduce(
             (total, [abbr, amount]) => {
                 return (
-                    total + currency.convert(amount, abbr as keyof Rates, "gp")
+                    total +
+                    currency.convert(amount, abbr as CoinDenominations, "gp")
                 );
             },
             0
         );
-        setTotal(currency.convert(totalInGold, "gp", totalCurrency));
-    }, [purse, totalCurrency]);
+        setTotal(
+            currency.convert(
+                totalInGold,
+                "gp",
+                totalCurrency as CoinDenominations
+            )
+        );
+    }, [purse.values, totalCurrency]);
+
+    const handleTransferFromHigherDenomination = (
+        currentDenomination: CoinDenominations
+    ) => {
+        const index = denominations.indexOf(currentDenomination);
+        if (index <= 0) {
+            return; // No higher denomination exists
+        }
+
+        let higherIndex = index - 1;
+        let found = false;
+
+        while (higherIndex >= 0) {
+            const higherDenomination = denominations[higherIndex];
+
+            if (purse.values[higherDenomination] >= 1) {
+                const amountToAdd = currency.convert(
+                    1,
+                    higherDenomination,
+                    currentDenomination
+                );
+                purse.set({
+                    ...purse.values,
+                    [higherDenomination]: purse.values[higherDenomination] - 1,
+                    [currentDenomination]:
+                        purse.values[currentDenomination] + amountToAdd,
+                });
+                found = true;
+                break;
+            } else {
+                higherIndex--;
+            }
+        }
+
+        if (!found) {
+            return;
+        }
+    };
+
+    const tidyUp = () => {
+        let totalInIp = 0;
+        denominations.forEach((denomination) => {
+            totalInIp += purse.values[denomination] * valuesInIp[denomination];
+        });
+
+        const newPurse: { [key in CoinDenominations]: number } = {
+            mp: 0,
+            gp: 0,
+            sp: 0,
+            bp: 0,
+            cp: 0,
+            tp: 0,
+            ip: 0,
+        };
+
+        denominations.forEach((denomination) => {
+            const amount = Math.floor(totalInIp / valuesInIp[denomination]);
+            totalInIp -= amount * valuesInIp[denomination];
+            newPurse[denomination] = amount;
+        });
+
+        purse.set(newPurse);
+    };
 
     return (
         <main className="relative p-4 flex flex-col h-full ">
             <div className="grid grid-flow-row auto-rows-auto gap-4">
-                {Object.entries(currency.abbreviations).map(([abbr, name]) => (
-                    <div key={abbr} className="flex flex-row">
+                {denominations.map((abbr, index) => (
+                    <div key={abbr} className="flex flex-row items-center">
                         <input
                             id={abbr}
                             type="number"
-                            value={purse.values[abbr as keyof Rates]}
+                            value={purse.values[abbr]}
                             onChange={(e) =>
                                 handleCurrencyChange(
                                     abbr,
@@ -77,32 +165,48 @@ const PursePage = () => {
                         />
                         <label
                             htmlFor={abbr}
-                            className="flex bg-corduroy-200 dark:text-base-content w-12 items-center justify-center"
+                            className="flex bg-corduroy-200 dark:text-base-content w-12 h-full items-center justify-center"
                         >
                             {abbr}
                         </label>
+                        {index > 0 && (
+                            <button
+                                onClick={() =>
+                                    handleTransferFromHigherDenomination(abbr)
+                                }
+                                className="btn btn-sm btn-primary h-full"
+                            >
+                                <FontAwesomeIcon
+                                    fixedWidth={true}
+                                    icon={byPrefixAndName.fas["plus"]}
+                                />
+                            </button>
+                        )}
                     </div>
                 ))}
             </div>
             <div className="flex flex-grow items-center justify-center text-3xl">
                 {total.toLocaleString()} {totalCurrency}
             </div>
-            <div className="">
+            <div className="mb-4">
                 <select
                     value={totalCurrency}
                     onChange={(e) =>
-                        setTotalCurrency(e.target.value as keyof Rates)
+                        setTotalCurrency(e.target.value as CoinDenominations)
                     }
                     className="select w-full select-bordered focus-within:select-primary"
                 >
-                    {Object.entries(currency.abbreviations).map(
-                        ([abbr, name]) => (
-                            <option key={abbr} value={abbr}>
-                                {abbr} - {name}
-                            </option>
-                        )
-                    )}
+                    {denominations.map((abbr) => (
+                        <option key={abbr} value={abbr}>
+                            {abbr} - {currency.abbreviations[abbr]}
+                        </option>
+                    ))}
                 </select>
+            </div>
+            <div className="mb-4">
+                <button onClick={tidyUp} className="btn btn-primary w-full">
+                    Tidy Up
+                </button>
             </div>
         </main>
     );
