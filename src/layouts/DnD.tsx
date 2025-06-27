e9ffd50073f439adbf48ca29069db0aec76aac7c
Stu C1 <79088483+stuc-aerstudios@users.noreply.github.com>
2025-06-27 12:05:20 +0100

    Revert "Push"
    This reverts commit 7808ee48cb6d87929c1530af40dc56d420f92885.

--- src/layouts/DnD.tsx
+++ src/layouts/DnD.tsx
@@ -7,9 +7,9 @@ const DnD = () => {
 		<div className="relative h-[100dvh] w-full overflow-hidden">
 			<div className="fixed top-0 left-0 right-0 bottom-0 flex flex-col items-end justify-end p-5 sm:p-10 md:p-20 bg-gradient-to-br from-primary to-base-100">
 				<Logo className="h-20" />
-				{/* <p className="text-2xl font-extralight">
+				<p className="text-2xl font-extralight">
 					Ideas built beautifully<span className="text-primary">.</span>
-				</p> */}
+				</p>
 			</div>
 			<div className="h-[calc(100%-.5rem)] w-[calc(100%-.5rem)] sm:h-[calc(100%-1rem)] sm:w-[calc(100%-1rem)] md:h-[calc(100%-2rem)] md:w-[calc(100%-2rem)] m-1 sm:m-2 md:m-4 p-1 sm:p-2 md:p-4 flex flex-col gap-1 sm:gap-2 md:gap-4 overflow-hidden">
 				<DnDMenu />
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -17,9 +17,9 @@ const LandingPage = () => {
 		<main className="flex flex-col items-center justify-center h-full">
 			<div>
 				<Logo className="h-20" />
-				{/* <p className="text-2xl font-extralight">
+				<p className="text-2xl font-extralight">
 					Ideas built beautifully<span className="text-primary">.</span>
-				</p> */}
+				</p>
 			</div>
 		</main>
 	);
