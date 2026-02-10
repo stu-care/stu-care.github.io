2a962d23a1b148128bef935576c524aa6ae407e3
Stu Care <79088483+stuc-aerstudios@users.noreply.github.com>
2026-02-10 15:40:42 +0000

    Updated colours and home

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
-import LogoFull from "../assets/LogoFull";
 import LogoIcon from "../assets/LogoIcon";
 import LogoType from "../assets/LogoType";
+import { useApp } from "../contexts/AppContext";
 
 const LandingPage = () => {
-    return ( <main className="w-full h-screen flex flex-col items-center justify-center gap-4 p-8">
+    const { version } = useApp();
+
+    return ( <main className="w-full h-screen relative flex flex-col items-center justify-center gap-4 p-8">
         <LogoIcon className="w-24 absolute bottom-8 right-8 text-stone-300 hover:text-stone-700 transition-colors duration-500"/>
         <LogoType className="w-84 text-stone-300" fill={{ stu: "var(--color-stone-500)", dot: "var(--color-stone-500)" }}/>
+        <span className="cursor-default select-none text-xs font-extrabold text-stone-200 absolute bottom-8 left-8">v{version}</span>
     </main> );
 }
  
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import LogoFull from "../assets/LogoFull";
+import { useApp } from "../contexts/AppContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { byPrefixAndName } from "@awesome.me/kit-5a5002bf29/icons";
 
 const NotFoundPage = () => {
-    return ( <main className="">
-        <LogoFull className="w-96 text-primary"/>
-        <p>Nothing here sorry!</p>
-        <Link to="/" className="btn btn-link">Go back</Link>
-    </main> );
+    const { version } = useApp();
+
+    return (<main className="w-full h-screen relative flex flex-col items-center justify-center gap-4 p-8">
+        <LogoFull className="w-84 text-stone-300" fill={{ stu: "var(--color-stone-500)", icon: "var(--color-stone-700)", dot: "var(--color-stone-500)" }} />
+        <p className="font-[Noto_Serif] text-stone-500 text-2xl">Whoops, nothing here!</p>
+        <Link to="/" className="btn btn-circle btn-dash"><FontAwesomeIcon icon={byPrefixAndName.fas["arrow-left"]} /></Link>
+        <span className="cursor-default select-none text-xs text-stone-200 absolute bottom-8 left-8">v{version}</span>
+    </main>);
 }
- 
+
+
 export default NotFoundPage;
